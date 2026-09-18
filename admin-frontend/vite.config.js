db4e049be7d09d1d4acf5dbe75a57e29745c2298
Aleem <null.pointer.1.0.1.0.1@gmail.com>
2026-09-18 15:49:12 +0530

    fix: serve app self-contained with max compression and immutable 1yr cache for  bill and 100% reliability

--- admin-frontend/vite.config.js
+++ admin-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://tasky-d81s.vercel.app/admin/',
+  base: '/admin/',
   plugins: [react()],
   server: {
     port: 5176
