76c73be44b737e07564a5e6131c8310ad98a1456
Aleem <null.pointer.1.0.1.0.1@gmail.com>
2026-09-20 22:07:32 +0530

    Offload admin-frontend static JS/CSS assets to Vercel CDN for  Render bill

--- admin-frontend/vite.config.js
+++ admin-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/admin/',
+  base: 'https://tasky-d81s.vercel.app/',
   plugins: [react()],
   server: {
     port: 5176
